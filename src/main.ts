6534422400d6461691b3a8eb85514c0b3f790e99
CANOST-id <csinner31@gmail.com>
2026-04-19 23:04:37 +0200

    edited imports

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
+// Main CSS import
 import './styles/style.scss'
+
+// Importing page ts files
+import './pages/game'
+import './pages/game-over'
+import './pages/game-end'
+import './pages/settings'
+import './services/navigation'
