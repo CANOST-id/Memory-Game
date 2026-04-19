8f2d128d56403ded53322ca32e3ce4d4e96d29ec
CANOST-id <csinner31@gmail.com>
2026-04-19 23:04:18 +0200

    added navigation top navigate html files - basic functions

---
+++ src/services/navigation.ts
@@ -0,0 +1,15 @@
+function startSettings() {
+    window.location.href = 'settings.html';
+}
+
+function startGame() {
+    window.location.href = 'game.html';
+}
+
+function gameOver() {
+    window.location.href = 'game-over.html';
+}
+
+function gameEnd() {
+    window.location.href = 'game-end.html';
+}
