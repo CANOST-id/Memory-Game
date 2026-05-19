8f80530499dbbdfe831b1562227ae2a3f0b866bc
CANOST-id <csinner31@gmail.com>
2026-05-19 22:02:16 +0200

    added missing card image

--- src/services/cards.ts
+++ src/services/cards.ts
@@ -37,5 +37,6 @@ export const cardImages: Record<string, string[]> = {
         '/src/assets/cards/games-theme/square.png',
         '/src/assets/cards/games-theme/toad.png',
         '/src/assets/cards/games-theme/triangle.png',
+        '/src/assets/cards/games-theme/joystick.png'
     ]
 };
