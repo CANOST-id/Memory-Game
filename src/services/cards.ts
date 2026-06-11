05d5a3220000f18b440c7dc40a226fda45f68e8c
CANOST-id <csinner31@gmail.com>
2026-06-11 20:13:17 +0200

    add new export function - cardTemplate

--- src/services/cards.ts
+++ src/services/cards.ts
@@ -40,3 +40,16 @@ export const cardImages = {
         'assets/cards/games-theme/joystick.png'
     ]
 };
+
+export function cardTemplate(theme: string, imgSrc: string): string {
+    return `
+        <div class="card__inner">
+            <div class="card__back">
+                <img src="assets/cards/${theme}/da-logo.png" alt="">
+            </div>
+            <div class="card__front">
+                <img src="${imgSrc}" alt="card image">
+            </div>
+        </div>
+    `;
+}
